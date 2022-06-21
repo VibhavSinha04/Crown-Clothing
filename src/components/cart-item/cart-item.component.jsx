a7a1e7b86005f3c158c880c23cb762a49c07964d
Vibhav04 <vibhav.sinha@hotmail.com>
2022-06-21 23:18:20 +0530

    before deploying

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,18 @@
+import { CartItemContainer, Name, Details, Img } from "./cart-item.styles";
+
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <CartItemContainer>
+      <Img src={imageUrl} alt={`${name}`} />
+      <Details>
+        <Name>{name}</Name>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </Details>
+    </CartItemContainer>
+  );
+};
+
+export default CartItem;
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import CheckoutItems from "../../components/checkout-items/checkout-items.component";
+import {
+  CheckoutContainer,
+  Header,
+  HeaderBlock,
+  Total,
+} from "./checkout.styles";
+
+const Checkout = () => {
+  const { cartItems, cartTotal } = useContext(CartContext);
+
+  return (
+    <CheckoutContainer>
+      <Header>
+        <HeaderBlock>
+          <span>Product</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Description</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Quantity</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Price</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Remove</span>
+        </HeaderBlock>
+      </Header>
+
+      {cartItems.map((cartItem) => (
+        <CheckoutItems key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <Total>Total: ${cartTotal}</Total>
+    </CheckoutContainer>
+  );
+};
+
+export default Checkout;
