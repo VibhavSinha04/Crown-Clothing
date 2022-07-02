086ed99d30cac29f3bff95f960316cefc9da2824
Vibhav04 <vibhav.sinha@hotmail.com>
2022-07-02 22:17:29 +0530

    correcting index file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -24,8 +24,7 @@ root.render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  root
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
